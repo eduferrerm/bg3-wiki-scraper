59c321598278df918e2ce9adebc7864e3d396598
eduferrerm <ekferrerm@gmail.com>
2024-05-11 00:17:18 +0200

    Implemented weapon schema and file creation with the data

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Yeah
+# Baldurs Gate 3 Build Companion App
+
+###### Todo
+
+weapon schema needs to include:
+
+- act where its found
+- url to its wiki page
--- index.js
+++ index.js
@@ -1,39 +1,60 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
 
-const url = "https://bg3.wiki/wiki/Greatswords";
-
-// const main = async () => {
-// 	const browser = await puppeteer.launch();
-// 	const page = await browser.newPage();
-// 	await page.goto(url);
-// 	await page.screenshot({ path: "screenshot.png" });
-// 	await browser.close();
-// };
-
-const main = async () => {
+const weaponsSchema = async (weaponType) => {
+	const url = `https://bg3.wiki/wiki/${weaponType}`;
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto(url);
 
-	const allRows = await page.evaluate(() => {
-		const rowItem = document.querySelectorAll("tr");
+	const weaponList = await page.evaluate(() => {
+		const header = document.querySelectorAll("th");
+		const rows = document.querySelectorAll("td");
+		let rowCounter = 0;
 
-		return Array.from(rowItem).map((item) => {
+		let schema = [];
+		let schemaItem = {};
+
+		const schemaKeys = Array.from(header)
+			.splice(0, 7)
+			.map((item) => {
+				const text = item.innerText;
+				return text;
+			});
+
+		const schemaValues = Array.from(rows).map((item) => {
 			const text = item.innerText;
-			// const url = item.href;
-			return { text };
+			return text;
 		});
+
+		for (i = 0; i < schemaValues.length; i++) {
+			if (rowCounter === 7) {
+				rowCounter = 0;
+				schema.push(schemaItem);
+				schemaItem = {};
+			}
+			schemaItem[schemaKeys[rowCounter]] = schemaValues[i];
+			rowCounter++;
+		}
+		return schema;
 	});
 
-	const parsedData = JSON.stringify(allRows);
+	return weaponList;
+};
 
-	fs.writeFile("scraped-content.json", parsedData, function (err) {
+const greatswordData = weaponsSchema("Greatswords");
+
+greatswordData.then((res) => {
+	writeJsonFile("greatswords-schema", res);
+});
+
+const writeJsonFile = (fileName, data) => {
+	const parsedData = JSON.stringify(data);
+	console.log({ parsedData });
+	fs.writeFile(`${fileName}.json`, parsedData, function (err) {
 		if (err) {
 			return console.log(err);
 		}
 		console.log("The file was saved!");
 	});
 };
-
-main();
